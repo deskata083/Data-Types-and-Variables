3eb9ffd9c0ce1db0211fc9ed59ff3cd194e9506a
deskata083 <deskata083@gmail.com>
2017-10-04 13:26:16 +0300

    Add files via upload

---
+++ 19.TheaThePhotographer.cs
@@ -0,0 +1,33 @@
+﻿namespace TheaThePhotographer
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ulong numberOfPic = ulong.Parse(Console.ReadLine());
+            ulong filterTime = ulong.Parse(Console.ReadLine());
+            ulong percent = ulong.Parse(Console.ReadLine());
+            ulong uploadTime = ulong.Parse(Console.ReadLine());
+
+            ulong filteredPic =(ulong) Math.Ceiling(numberOfPic * percent / 100.0);
+            //int totalFTime = numberOfPic * filterTime;
+            //int totalUploadTime = filteredPic * uploadTime;
+            //int totalTime = totalFTime + totalUploadTime;
+
+            ulong totalTime = (numberOfPic * filterTime) + (filteredPic * uploadTime);
+
+            ulong days = totalTime / 86400;
+            ulong sec = totalTime % 86400;
+            ulong hours = sec / 3600;
+            sec = sec % 3600;
+            ulong min = sec / 60;
+            sec = sec % 60;
+           // double sec = hours % 3600;
+            
+            Console.WriteLine("{0}:{1:00}:{2:00}:{3:00}", 
+                 days ,hours, min, sec);
+
+        }
+    }
+}
